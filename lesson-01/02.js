eacc6edccdac20e48ee0bc4952ae38a3ad3ca7e4
molnia_grom <mgrom250@gmail.com>
2024-09-12 11:04:34 +0300

    task 2 solved (changes)

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -4,5 +4,5 @@ let number1 = Math.floor(Math.random() * 100);
   if (number1 % 2 === 0) {
       console.log(number1, "Это четное число");
   } else{
-      console.log(number1, "Это не четное число");
+      console.log(number1, "Это нечетное число");
   }
