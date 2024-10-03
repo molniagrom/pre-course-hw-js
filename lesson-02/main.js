da323c30464a78a09841fd6741f54b307e84a82f
molnia_grom <mgrom250@gmail.com>
2024-10-03 14:15:25 +0300

    I guess that's it

--- lesson-02/main.js
+++ lesson-02/main.js
@@ -1,4 +1,5 @@
-import { a } from "./data.module.js";
-import { render } from "./render.module.js";
-
-render(a);
+import { a } from "./data.module.js"; 
+import { render } from "./render.module.js"; 
+console.log("main, start"); 
+render(a); 
+console.log("main, finish");
