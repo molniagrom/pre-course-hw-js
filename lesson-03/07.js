c7f5669d7ed2190adf3477fba84e87a8db49e786
molnia_grom <mgrom250@gmail.com>
2024-10-21 17:58:18 +0300

    Homework attempt 7

---
+++ lesson-03/07.js
@@ -0,0 +1,10 @@
+const numbers = [
+  [1, 2, 3, 4, 5],
+  [11, 20, 33, 40, 55],
+  [111, 200, 333, 400, 555],
+];
+
+const removeOddNumbers = numbers.map((arr) => arr.filter((obj) => obj % 2 === 0));
+
+console.log(removeOddNumbers);
+
