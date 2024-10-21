c7eda0adbdfd086133fb9f17094d01cca8ce908f
molnia_grom <mgrom250@gmail.com>
2024-10-21 12:25:10 +0300

    Homework attempt 2

---
+++ lesson-03/02.js
@@ -0,0 +1,9 @@
+const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
+
+const foundOrange = words.indexOf("апельсин");
+
+if (foundOrange){
+  console.log("Ура! нашел");
+} else{
+  console.log("Придется поискать в другом магазине…");
+}
