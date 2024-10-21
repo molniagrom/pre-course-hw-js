36f478e77be118d10408ccdf539a17cd70ce6dbf
molnia_grom <mgrom250@gmail.com>
2024-10-21 12:16:23 +0300

    Homework attempt 1

---
+++ lesson-03/01.js
@@ -0,0 +1,11 @@
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+  return colors
+    .filter(
+      (elem) => elem === "черный" || elem === "красный" || elem === "желтый"
+    )
+    .join("-");
+}
+
+console.log(createColorString());
